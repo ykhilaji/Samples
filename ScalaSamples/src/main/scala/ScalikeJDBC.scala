d99749a2fb197d8dd7c42c1150038d578f71f99a
Nick <grifon52@gmail.com>
2018-02-18 22:21:12 +0300

    ScalikeJDBC
    additional samples

--- ScalaSamples/src/main/scala/ScalikeJDBC.scala
+++ ScalaSamples/src/main/scala/ScalikeJDBC.scala
@@ -13,10 +13,55 @@ object Entity extends SQLSyntaxSupport[Entity] {
 
 object ScalikeJDBC extends App {
   Class.forName("org.postgresql.Driver")
-  ConnectionPool.singleton("jdbc:postgresql://192.168.99.100:5432/sa", "sa", "sa")
+
+  val settings = ConnectionPoolSettings(
+    initialSize = 5,
+    maxSize = 10,
+    connectionTimeoutMillis = 1000,
+    validationQuery = "select 1"
+  )
+
+  GlobalSettings.queryCompletionListener = (query, params, count) => {
+    println(s"Query completed. $query, ${params mkString ","}, $count")
+  }
+
+  GlobalSettings.queryFailureListener = (query, params, error) => {
+    println(s"Query failed. $query, ${params mkString ","}, $error")
+  }
+
+  GlobalSettings.loggingSQLAndTime = LoggingSQLAndTimeSettings(
+    enabled = true,
+    singleLineMode = true,
+    logLevel = 'DEBUG
+  )
+
+  ConnectionPool.singleton(
+    url = "jdbc:postgresql://192.168.99.100:5432/sa",
+    user = "sa",
+    password = "sa",
+    settings = settings)
+
   implicit val session = AutoSession
 
   sql"select 'testSelect' ".foreach(x => println(x.get[String](1)))
 
   sql"select * from sync.entity".map(rs => Entity(rs)).list.apply().foreach(println)
+
+  DB readOnly {
+    implicit session => sql"select 'readOnly single result'".map(rs => rs.get[String](1)).single.apply().foreach(println)
+  }
+
+  DB readOnly {
+    implicit session =>
+      session.fetchSize(1)
+      sql"select * from sync.entity".map(rs => Entity(rs)).first.apply().foreach(println)
+  }
+
+  val id = 1
+  DB localTx {
+    implicit session =>
+      sql"update sync.entity set value='updatedValue' where id=${id}".update().apply()
+  }
+
+  sql"select 1/0".execute().apply()
 }
