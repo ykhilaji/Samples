4be19c960c753d5cf86e22f521f1e2e558435deb
Nick <ryanov.nikita@gmail.com>
2018-11-05 02:17:29 +0300

    More shapeless basic samples
    (or one more attempt to understand shapeless ^_^)

--- ScalaSamples/shapeless/src/test/scala/ShapelessTests.scala
+++ ScalaSamples/shapeless/src/test/scala/ShapelessTests.scala
@@ -1,5 +1,6 @@
 import org.scalatest.{FunSuite, Matchers}
 import shapeless._
+import shapeless.ops.hlist._
 
 class ShapelessTests extends FunSuite with Matchers {
   test("shapeless list") {
@@ -37,5 +38,66 @@ class ShapelessTests extends FunSuite with Matchers {
     val aLens = lens[A].a
     val a = A(1, "2", 3.0)
     assert(A(2, "2", 3.0) == aLens.set(a)(2))
+  }
+
+  test("zip with index") {
+    val l = "a" :: "b" :: "c" :: HNil
+    implicit val z = new Zip[String :: String :: String :: HNil] {
+      override type Out = (String, Int) :: (String, Int) :: (String, Int) :: HNil
+
+      override def apply(t: String :: String :: String :: HNil): ::[(String, Int), (String, Int) :: (String, Int) :: HNil] = (t.head, 1) :: (t.tail.head, 2) :: (t.tail.tail.head, 3) :: HNil
+
+    }
+    val zipped = l.zip
+    assert(("a",1) :: ("b",2) :: ("c",3) :: HNil == zipped)
+  }
+
+  test("HList to List using function") {
+    trait A
+    case class B(a: Int) extends A
+    case class C(a: Int, b: String, c: Double) extends A
+    def foo[H <: HList, T <: A](a: T)(implicit gen: Generic.Aux[T, H], toTraversable: ToTraversable.Aux[H, List, Any]): List[Any] = gen.to(a).toList
+    val b = B(1)
+    val c = C(1, "2", 3.0)
+    assert(List(1) == foo(b))
+    assert(List(1, "2", 3.0) == foo(c))
+  }
+
+  test("HList to mapped List") {
+    case class B(a: Int)
+    case class C(a: Int, b: String, c: Double, d: Float)
+    trait lowPriorityToStr extends Poly1 {
+      implicit def default[A] = at[A](_ => "some LP string")
+    }
+
+    object toStr extends lowPriorityToStr {
+      implicit val atInt = at[Int](_ => "some string from int")
+      implicit val atString = at[String](_ => "some string from string")
+      implicit val atDouble = at[Double](_ => "some string from double")
+    }
+
+    val genB = Generic[B]
+    val genC = Generic[C]
+    val b = B(1)
+    val c = C(1, "2", 3.0, 4)
+
+    assert(List("some string from int") == genB.to(b).map(toStr).toList)
+    assert(List("some string from int", "some string from string", "some string from double", "some LP string") == genC.to(c).map(toStr).toList)
+  }
+
+  test("map hlist using function") {
+    case class A(a: Int, b: Int, c: Int)
+    trait lowPriorityMap extends Poly1 {
+      implicit def default[A] = at[A](identity)
+    }
+
+    object map extends lowPriorityMap {
+      implicit def caseInt = at[Int](i => i * 2)
+    }
+
+    def foo[H <: HList, L <: HList](a: A)(implicit gen: Generic.Aux[A, H], mapper: Mapper.Aux[map.type, H, L]) = gen.to(a).map(map)
+
+    val a = A(1, 2, 3)
+    assert(2 :: 4 :: 6 :: HNil == foo(a))
   }
 }
